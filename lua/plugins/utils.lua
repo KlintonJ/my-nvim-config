5373f02ea851b90573ca488be8f1e29321384bdc
klintonjohnson <157005839+KlintonJ@users.noreply.github.com>
2025-05-18 12:43:56 -0500

    feat(which-key): adds folke's which-key plugin

--- lua/plugins/utils.lua
+++ lua/plugins/utils.lua
@@ -20,5 +20,28 @@ return {
 		opts = { use_default_keymaps = false, max_join_length = 150 },
 	},
 
-	-- next goes here
+	-- folke/which-key
+	{
+		"folke/which-key.nvim",
+		event = "VeryLazy",
+		opts = {
+			-- your configuration comes here
+			-- or leave it empty to use the default settings
+			-- refer to the configuration section below
+		},
+		keys = {
+			{
+				"<leader>?",
+				function()
+					require("which-key").show({
+						global = false,
+						keys = "<c-w>",
+						loop = true,
+					})
+				end,
+				desc = "Buffer Local Keymaps (which-key)",
+			},
+		},
+	},
+
 }
